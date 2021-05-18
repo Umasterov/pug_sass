01b86dbb06870170e94f68998977172bb581deb9
umasterov <umasterov@mail.ru>
2021-05-18 20:15:11 +0300

    Fix deploy error

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-import { module } from 'module.js';
+//import { module } from 'module.js';
 //var importCdn = document.createElement('script');
 //importCdn.setAttribute('src', '//cdn.serp.com/4654321324654321346784gfgcb21gfh4t1231b6tsrb1_0.js');
 //document.head.appendChild(importCdn);
